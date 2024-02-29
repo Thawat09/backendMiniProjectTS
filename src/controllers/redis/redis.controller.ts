bdcf641917c467378dfab9e67bf5379302efbe35
Thawat <thawat@dtc.co.th>
2024-02-29 17:28:11 +0700

    Fixed calling Edit validationResult correctly.
    Fixed calling Edit validationResult correctly.

--- src/controllers/redis/redis.controller.ts
+++ src/controllers/redis/redis.controller.ts
@@ -1,23 +1,11 @@
 //TODO นำเข้าชนิดของข้อมูล
 import { Request, Response } from 'express'; // นำเข้าชนิดของข้อมูล Request และ Response จาก Express framework
 
-//TODO นำเข้าฟังก์ชัน
-import { validationResult } from 'express-validator'; // นำเข้าฟังก์ชัน validationResult จาก express-validator เพื่อตรวจสอบความถูกต้องของข้อมูล
-
 //TODO นำเข้าเส้นทาง
 import redisHelper from '../../helpers/redis/redis.helper' // redis helper
 
 //TODO ฟังก์ชัน indexController ที่จะใช้ในการควบคุมการทำงานของเส้นทาง
 export const getAll = async (req: Request, res: Response) => {
-    // ตรวจสอบความถูกต้องของข้อมูลที่ถูกส่งมากับคำขอ
-    const errors = validationResult(req);
-
-    // หากมีข้อผิดพลาด
-    if (!errors.isEmpty()) {
-        // ส่งคำตอบกลับด้วยสถานะ 400 (Bad Request) พร้อมกับข้อผิดพลาดทั้งหมด
-        return res.status(400).json({ errors: errors.array() });
-    }
-
     const key: string = req.body.key;
 
     try {
@@ -30,15 +18,6 @@ export const getAll = async (req: Request, res: Response) => {
 }
 
 export const getKey = async (req: Request, res: Response) => {
-    // ตรวจสอบความถูกต้องของข้อมูลที่ถูกส่งมากับคำขอ
-    const errors = validationResult(req);
-
-    // หากมีข้อผิดพลาด
-    if (!errors.isEmpty()) {
-        // ส่งคำตอบกลับด้วยสถานะ 400 (Bad Request) พร้อมกับข้อผิดพลาดทั้งหมด
-        return res.status(400).json({ errors: errors.array() });
-    }
-
     const key: string = req.body.key;
 
     try {
